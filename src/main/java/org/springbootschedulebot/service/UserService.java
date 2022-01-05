077d343d0b46a5260ba91686323f8da200483a4e
PAKETA-0629 <kyrylo0629@gmail.com>
2022-01-05 15:27:44 +0200

    add set state and set stage

--- src/main/java/org/springbootschedulebot/service/UserService.java
+++ src/main/java/org/springbootschedulebot/service/UserService.java
@@ -2,6 +2,7 @@ package org.springbootschedulebot.service;
 
 import org.springbootschedulebot.model.User;
 import org.springbootschedulebot.repository.UserRepository;
+import org.springbootschedulebot.util.States;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,5 +32,13 @@ public class UserService {
 
     public User findByName(String username) {
         return userRepository.findByName(username);
+    }
+
+    public void setState(User user, States state, Long stage) {
+        userRepository.updateState(user.getId(), state.name(), stage);
+    }
+
+    public void setStage(User user, Long stage) {
+        userRepository.updateStage(user.getId(), stage);
     }
 }
---
+++ src/test/java/org/springbootschedulebot/repositoryIT/UserRepositoryIT.java
@@ -0,0 +1,2 @@
+package org.springbootschedulebot.repositoryIT;public class UserRepositoryIT {
+}
